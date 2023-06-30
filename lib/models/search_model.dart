2c86511ad25cf16da3b25a9f8807f8e128c048e1
HUYNH Henry <henry018@laposte.net>
2023-06-30 10:40:56 +0200

    search model

---
+++ lib/models/search_model.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+enum SearchState {
+  open,
+  discard,
+  search,
+  searched,
+  found
+}
+
+class SearchModel {
+  SearchModel(this.value);
+
+  final int value;
+  SearchState state = SearchState.open;
+  Color color = Colors.black54;
+  GlobalKey key = GlobalKey();
+
+  void reset() {
+    state = SearchState.open;
+    color = Colors.black54;
+  }
+
+  void potential() {
+    state = SearchState.search;
+    color = Colors.blue;
+  }
+
+  void discard() {
+    state = SearchState.discard;
+    color = Colors.red;
+  }
+
+  void found() {
+    state = SearchState.found;
+    color = Colors.green;
+  }
+
+  void searched() {
+    state = SearchState.searched;
+  }
+}
